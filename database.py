bdeeac3bb98278c75e03d7fefa595f910579cfbb
abboabbo <ryanabbott1984@icloud.com>
2026-06-11 18:01:41 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import streamlit as st
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = st.secrets["postgresql://postgres.dvpzvzznzskfflrcmupy:O9kfq99e1!!@aws-1-eu-central-1.pooler.supabase.com:6543/postgres"]
+DATABASE_URL = st.secrets["DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL,
