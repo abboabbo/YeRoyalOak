3555e606f60784c45bc76c98f1acdb4d97069df3
abboabbo <ryanabbott1984@icloud.com>
2026-07-17 13:27:11 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -767,8 +767,7 @@ if not st.session_state.logged_in:
 
         st.markdown(
             """
-             <h1 style="text-align:center; margin-bottom:0;">
-                Ye Royal Oak Darts League
+            <h1 style="text-align:center; margin-bottom:0;">
             </h1>
 
             <h1 style="
